88f3209ba081c375b2a42ec16bd9f7a021f1344e
Luan Lemos <ipsluan@icloud.com>
2024-12-14 17:49:15 -0300

    feat(mobile): Home

---
+++ mobile/src/components/step/index.tsx
@@ -0,0 +1,24 @@
+import { View, Text } from "react-native";
+import { IconProps } from "@tabler/icons-react-native";
+
+import { colors } from "@/styles/theme";
+import { s } from "./styles";
+
+interface StepProps {
+  title: string;
+  description: string;
+  icon: React.ComponentType<IconProps>;
+}
+
+export function Step({ title, description, icon: Icon }: StepProps) {
+  return (
+    <View style={s.container}>
+      {/* <Icon size={32} color={colors.red.base} /> */}
+
+      <View style={s.details}>
+        <Text style={s.title}>{title}</Text>
+        <Text style={s.description}>{description}</Text>
+      </View>
+    </View>
+  );
+}
